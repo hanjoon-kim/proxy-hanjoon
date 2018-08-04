3b0289272ae712879a085c93a03b0d359af0905e
Hanjoon Kim <hanjoon3@gmail.com>
2018-08-04 16:39:19 -0700

    Implement proxy with one database

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
-const port = 3000;
+const mysql = require('mysql');
 
 const app = express();
+const port = 3000;
+
+const con = mysql.createConnection({
+  user: 'root',
+  password: 'password',
+  database: 'similarproducts',
+});
+
+app.use(bodyParser.json());
 app.use(express.static('public'));
+
+app.get('/related/api', function (req, res) {
+  con.connect((err) => {
+    if (err) {
+      console.log('Error connecting to MySql');
+      return;
+    }
+    console.log('Connected to database');
+  })
+
+  var query = 'SELECT * FROM products';
+
+  con.query(query, function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Query successful');
+      res.send(result);
+    }
+  })
+})
+
+// app.get(`*`, (req, res) => {
+//   res.sendFile(path.resolve(__dirname, `./public`, `index.html`));
+// })
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
-//
